d7e3992e77e7b718ae2efa2981c0f073cd8a01e9
maedeh zangeneh <zangeneh5132@gmail.com>
2024-10-30 20:51:25 +0330

    add redux2.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, reset } from "./redux/actions";
+import './App.css'; 
 function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <div className="buttons">
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
     </div>
   );
 }
